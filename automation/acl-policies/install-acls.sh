9c590973b6ee5d2b3e210b0015534e0d25296a4f
Glenn Wiseman <gwiseman@cumulusnetworks.com>
2019-07-17 14:39:55 -0500

    updated acl script

--- automation/acl-policies/install-acls.sh
+++ automation/acl-policies/install-acls.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ssh cumulus@internet "mkdir -p ./acl-policies"
 scp * cumulus@internet:~/acl-policies
+ssh cumulus@internet "sudo cp ~/acl-policies/* /etc/cumulus/acl/policy.d/."
+ssh cumulus@internet "sudo cl-acltool -i"
